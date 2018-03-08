8f7d7f09baf1aa2b9fd99f0219fb43e690d4e5a7
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2018-03-08 16:27:56 +0100

    controllers, routes and destroys done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
 
-  get 'cocktails/show'
 
-  get 'cocktails/new'
-
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+      resources :doses, only: [ :new, :create, :destroy ]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
